407dbe48b2a4b9b9bcaf040c2c1d2d45a6521947
DevBielgrazi <gabrielvieira.tcc@gmail.com>
2022-05-13 11:48:07 -0300

    Relatório diário

---
+++ financeiro/relatorio_diario.php
@@ -0,0 +1,119 @@
+<?php
+session_start();
+if(!isset($_SESSION["system_control"])){
+?>
+	<script>
+		document.location.href="http://localhost/transluccaggi/index_financeiro.html";
+	</script>
+<?php
+}else{
+	$system_control = $_SESSION["system_control"];
+	if($system_control == 2){
+?>
+<html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+		<link rel="icon" href="..\imagem/favicone.png"/>
+		<link href="..\estilo.css" rel="stylesheet"/>
+		<title>Matriz Principal</title>
+	</head>
+	<body>
+        <menu>
+            <a href="http://localhost/transluccaggi/menu.php"><img src="..\imagem/logo.png" width=20%></a>
+            <h1>MATRIZ PRINCIPAL</h1><p>
+            <a href="http://localhost/transluccaggi/logout.php"><img src="..\imagem/exit.png" width=3%></a>
+            <table class="tableb">
+                <tr><td><a href="../saida/form_saida_motorista.php"><button class="buttonb">SAÍDA DE MOTORISTAS</button></a></td></tr>
+                <tr><td><a href="../saida/form_baixa_canhotos.php"><button class="buttonb">BAIXA DE CANHOTOS</button></a></td></tr>
+                <tr><td><a href="../saida/form_romaneio_cargas.php"><button class="buttonb">ROMANEIO DE CARGAS</button></a></td></tr>
+                <tr><td><a href="../saida/form_registro_devolucao.php"><button class="buttonb">REGISTRO DE DEVOLUÇÕES</button></a></td></tr>
+                <tr><td><h2>CADASTROS</h2></td></tr>
+                <tr><td><a href="..\cadastro/form_cadastrar_nfs.php"><button>NOTAS</button></a></td></tr>
+                <tr><td><a href="..\cadastro/form_cadastrar_clientes.php"><button>CLIENTES</button></a></td></tr>
+                <tr><td><a href="..\cadastro/form_cadastrar_distribuidoras.php"><button>DISTRIBUIDORAS</button></a></td></tr>
+                <tr><td><h2>PESQUISAS</h2></td></tr>
+                <tr><td><a href="..\pesquisa/form_pesquisar_nfs.php"><button>NOTAS</button></a></td></tr>
+                <tr><td><a href="..\pesquisa/form_pesquisar_clientes.php"><button>CLIENTES</button></a></td></tr>
+                <tr><td><a href="..\pesquisa/form_pesquisar_distribuidoras.php"><button>DISTRIBUIDORAS</button></a></td></tr>
+                <tr><td><h2>MOTORISTAS</h2></td></tr>
+                <tr><td><a href="..\cadastro/form_cadastrar_motoristas.php"><button>CADASTRAR</button></a></td></tr>
+                <tr><td><a href="..\pesquisa/form_pesquisar_motoristas.php"><button>PESQUISAR</button></a></td></tr>
+                <tr><td><h2>FINANCEIRO</h2></td></tr>
+                <tr><td><a href="..\financeiro/form_relatorio_mensal.php"><button>RELATÓRIO MENSAL</button></a></td></tr>
+                <tr><td><a href="..\financeiro/form_frete_motoristas.php"><button>FRETE MOTORISTAS</button></a></td></tr>
+                <tr><td><a href="..\financeiro/form_fechamento_distribuidoras.php"><button>FECHAMENTO DISTRIBUIDORAS</button></a></td></tr>
+                <tr><td><a href="..\financeiro/form_fechamento_motoristas.php"><button>FECHAMENTO MOTORISTAS</button></a></td></tr>
+            </table>
+        </menu>
+        <rn>
+<?php
+#VARIÁVEIS DO FORMULÁRIO
+$dat_rel = trim($_POST['dat_rel']);
+
+require('../connect.php');
+#ADQUIRINDO INFORMAÇÕES DO BANCO
+
+$sql = mysqli_query($conn,"SELECT * FROM $tab_dis");
+$n = mysqli_num_rows($sql);
+
+$fre_disg = 0;
+for($i=1; $i<$n; $i++){
+    $sql = mysqli_query($conn,"SELECT `porcentagem` as 'por' FROM $tab_dis WHERE `codigo` = '$i'");
+    $sql = mysqli_fetch_array($sql);
+    $por = (float) $sql['por'];
+    if($i==1){
+        $sql = mysqli_query($conn,"SELECT SUM(`valor`) as 'val' FROM $tab_nfs WHERE `cod_distribuidora` = '$i' AND `emissao` = '$dat_rel'");
+    }
+    else{
+        $sql = mysqli_query($conn,"SELECT SUM(`valor`) as 'val' FROM $tab_nfs WHERE `cod_distribuidora` = '$i' AND `entrada` = '$dat_rel'");
+    }
+    $sql = mysqli_fetch_array($sql);
+    $val_mer = (float) $sql['val'];
+    $fre_dis = ($por * $val_mer)/100;
+    $fre_disg = $fre_disg+$fre_dis;
+}
+$fre_disg = number_format(($fre_disg), 2, '.', '');
+
+$sql = mysqli_query($conn,"SELECT SUM(`valor`) as 'fre' FROM $tab_fre WHERE `data` = '$dat_rel'");
+$sql = mysqli_fetch_array($sql);
+$fre_mot = number_format(($sql['fre']), 2, '.', '');
+
+$sal_men = $fre_disg - $fre_mot;
+$sal_men = number_format(($sal_men), 2, '.', '');
+?>
+		<rf>
+			<table border=1>
+				<h1>RELATÓRIO DIÁRIO</h1>
+				<tr>
+					<td><h3>DATA</h3></td>
+					<td><h3>VALOR FRETES</h3></td>
+					<td><h3>CUSTO</h3></td>
+					<td><h3>SALDO</h3></td>
+					<td><h3>STATUS</h3></td>
+				</tr>
+                <tr>
+					<td><h4><nobr><?php echo date( 'd/m/Y' , strtotime( $dat_rel));   ?><nobr></h4></td>
+					<td><h4><nobr><?php echo "R$".$fre_disg;    ?><nobr></h4></td>
+					<td><h4><nobr><?php echo "R$".$fre_mot;    ?><nobr></h4></td>
+					<td><h4><nobr><?php echo "R$".$sal_men;    ?><nobr></h4></td>
+<?php if($sal_men<0){?><td><h6><nobr>NEGATIVO</nobr></h6></td><?php
+        }else{?><td><h7><nonr>POSITIVO</nobr></h7></td><?php
+        }?>
+				</tr>
+			</table>
+</rf>
+	</body>
+</html>
+<?php
+    }
+    
+    else
+    {
+?>
+	<script>
+		document.location.href="http://localhost/transluccaggi/financeiro/index_financeiro.html";
+	</script>
+<?php
+    }
+}
+?>
