3a8f38e3253cb2d75eb6743674a4d477e9e09a23
DevBielgrazi <gabrielvieira.tcc@gmail.com>
2022-07-25 15:21:44 -0300

    Readicionando connect

---
+++ connect.php
@@ -0,0 +1,15 @@
+<?php
+#CONEXÃO COM O SERVIDOR
+	$conn = mysqli_connect("localhost", "root", "") or die("Não foi possível a conexão com o Banco");
+#SELEÇÃO COM O BANCO
+	$db = mysqli_select_db($conn,"transluccaggi_bd") or die("Não foi possível selecionar o Banco");
+#TABELAS DO BANCO
+	$tab_nfs = "notas_fiscais";
+	$tab_cli = "clientes";
+	$tab_dis = "distribuidoras";
+	$tab_mot = "motoristas";
+	$tab_usu = "usuarios";
+	$tab_fre = "fretes";
+	$tab_cus = "custos";
+	$tab_dev = "devolucoes";
+ ?>
