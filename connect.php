8d60e69dcc5ee19d476e002f5bed392f45aa6264
DevBielgrazi <gabrielvieira.tcc@gmail.com>
2022-07-26 12:33:36 -0300

    Aumentando Fontes

--- connect.php
+++ connect.php
@@ -2,7 +2,7 @@
 #CONEXÃO COM O SERVIDOR
 	$conn = mysqli_connect("localhost", "root", "") or die("Não foi possível a conexão com o Banco");
 #SELEÇÃO COM O BANCO
-	$db = mysqli_select_db($conn,"transluccaggi_bd2") or die("Não foi possível selecionar o Banco");
+	$db = mysqli_select_db($conn,"transluccaggi_bd") or die("Não foi possível selecionar o Banco");
 #TABELAS DO BANCO
 	$tab_nfs = "notas_fiscais";
 	$tab_cli = "clientes";
