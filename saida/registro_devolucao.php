594a6032156d7340f6b3915f7473764718016179
DevBielgrazi <gabrielvieira.tcc@gmail.com>
2022-09-01 16:41:46 -0300

    Adicionando icones no menu

--- saida/registro_devolucao.php
+++ saida/registro_devolucao.php
@@ -20,57 +20,58 @@ if(!isset($_SESSION["system_control"])){
 		<title>Matriz Principal</title>
 	</head>
 	<body>
-		<div class="bar">
+    <div class="bar">
 			<div class="dropdown">
-        <img onclick="myFunction()"class="dropbtn" src="..\imagem/bars.png" width="2%"></img>
-            <div id="myDropdown" class="dropdown-content">
-                <a href="form_saida_motorista.php">>SAÍDA DE MOTORISTAS</a>
-                <a href="baixa_canhotos.php">>BAIXA DE CANHOTOS</a>
-                <a href="form_romaneio_cargas.php">>ROMANEIO DE CARGAS</a>
-                <a href="registro_devolucao.php">>REGISTRO DE DEVOLUÇÕES</a>
-				<a href="agendar_entrega.php">>AGENDAR ENTREGA</a>
-                <a href="..\cadastro/cadastrar_nfs.php">>CADASTRO NOTAS</a>
-                <a href="..\cadastro/cadastrar_clientes.php">>CADASTRO CLIENTES</a>
-                <a href="..\cadastro/cadastrar_distribuidoras.php">>CADASTRO DISTRIBUIDORAS</a>
-                <a href="..\pesquisa/form_pesquisar_nfs.php">>PESQUISAR NOTAS</a>
-                <a href="..\pesquisa/form_pesquisar_clientes.php">>PESQUISAR CLIENTES</a>
-                <a href="..\pesquisa/form_pesquisar_distribuidoras.php">>PESQUISAR DISTRIBUIDORAS</a>
-                <a href="..\cadastro/cadastrar_motoristas.php">>CADASTRAR MOTORISTA</a>
-                <a href="..\pesquisa/form_pesquisar_motoristas.php">>PESQUISAR MOTORISTA</a>
-                <a href="..\financeiro/form_relatorio_diario.php">>RELATÓRIO DIÁRIO</a>
-                <a href="..\financeiro/form_relatorio_diario_cidades.php">>RELATÓRIO DIÁRIO CIDADES</a>
-                <a href="..\financeiro/form_relatorio_mensal.php">>RELATÓRIO MENSAL</a>
-                <a href="..\financeiro/form_relatorio_mensal_cidades.php">>RELATÓRIO MENSAL CIDADES</a>
-                <a href="..\financeiro/form_relatorio_anual.php">>RELATÓRIO ANUAL</a>
-                <a href="..\financeiro/form_frete_motoristas.php">>FRETE MOTORISTAS</a>
-                <a href="..\financeiro/form_fechamento_distribuidoras.php">>FECHAMENTO DISTRIBUIDORAS</a>
-                <a href="..\financeiro/form_fechamento_motoristas.php">>FECHAMENTO MOTORISTAS</a>
-            </div>
-        </div>
-        <script>
-            function myFunction() {
-            document.getElementById("myDropdown").classList.toggle("show");
-            }
+        		<img onclick="myFunction()"class="dropbtn" src="..\imagem/bars.png" width="2%"></img>
+				<div id="myDropdown" class="dropdown-content">
+					<a href="..\saida/form_saida_motorista.php"><img src=..\imagem/saida.png> >SAÍDA DE MOTORISTAS</img></a>
+					<a href="..\saida/baixa_canhotos.php"><img src=..\imagem/baixa.png>>BAIXA DE CANHOTOS</a>
+					<a href="..\saida/form_romaneio_cargas.php"><img src=..\imagem/romaneio.png>>ROMANEIO DE CARGAS</a>
+					<a href="..\saida/registro_devolucao.php"><img src=..\imagem/devolucao.png>>REGISTRO DE DEVOLUÇÕES</a>
+					<a href="..\saida/agendar_entrega.php"><img src=..\imagem/agendar.png>>AGENDAR ENTREGA</a>
+					<a href="..\cadastro/cadastrar_nfs.php"><img src=..\imagem/cad_not.png>>CADASTRO NOTAS</a>
+					<a href="..\cadastro/cadastrar_clientes.php"><img src=..\imagem/cad_cli.png>>CADASTRO CLIENTES</a>
+					<a href="..\cadastro/cadastrar_distribuidoras.php"><img src=..\imagem/cad_dis.png>>CADASTRO DISTRIBUIDORAS</a>
+					<a href="..\pesquisa/form_pesquisar_nfs.php"><img src=..\imagem/pes_not.png>>PESQUISAR NOTAS</a>
+					<a href="..\pesquisa/form_pesquisar_clientes.php"><img src=..\imagem/pes_cli.png>>PESQUISAR CLIENTES</a>
+					<a href="..\pesquisa/form_pesquisar_distribuidoras.php"><img src=..\imagem/pes_dis.png>>PESQUISAR DISTRIBUIDORAS</a>
+					<a href="..\cadastro/cadastrar_motoristas.php"><img src=..\imagem/cad_mot.png>>CADASTRAR MOTORISTA</a>
+					<a href="..\pesquisa/form_pesquisar_motoristas.php"><img src=..\imagem/pes_mot.png>>PESQUISAR MOTORISTA</a>
+					<a href="..\financeiro/form_relatorio_diario.php"><img src=..\imagem/diario.png>>RELATÓRIO DIÁRIO</a>
+					<a href="..\financeiro/form_relatorio_diario_cidades.php"><img src=..\imagem/cid_dia.png>>RELATÓRIO DIÁRIO CIDADES</a>
+					<a href="..\financeiro/form_relatorio_mensal.php"><img src=..\imagem/mensal.png>>RELATÓRIO MENSAL</a>
+					<a href="..\financeiro/form_relatorio_mensal_cidades.php"><img src=..\imagem/cid_mes.png>>RELATÓRIO MENSAL CIDADES</a>
+					<a href="..\financeiro/form_relatorio_anual.php"><img src=..\imagem/anual.png>>RELATÓRIO ANUAL</a>
+					<a href="..\financeiro/form_relatorio_anual_cidades.php"><img src=..\imagem/cid_ano.png>>RELATÓRIO ANUAL CIDADES</a>
+					<a href="..\financeiro/form_frete_motoristas.php"><img src=..\imagem/fre_mot.png>>FRETE MOTORISTAS</a>
+					<a href="..\financeiro/form_fechamento_distribuidoras.php"><img src=..\imagem/fec_dis.png>>FECHAMENTO DISTRIBUIDORAS</a>
+					<a href="..\financeiro/form_fechamento_motoristas.php"><img src=..\imagem/fec_mot.png>>FECHAMENTO MOTORISTAS</a>
+            	</div>
+        	</div>
+			<script>
+				function myFunction() {
+				document.getElementById("myDropdown").classList.toggle("show");
+				}
 
-            window.onclick = function(event) {
-            if (!event.target.matches('.dropbtn')) {
-                var dropdowns = document.getElementsByClassName("dropdown-content");
-                var i;
-                for (i = 0; i < dropdowns.length; i++) {
-                var openDropdown = dropdowns[i];
-                if (openDropdown.classList.contains('show')) {
-                    openDropdown.classList.remove('show');
-                }
-                }
-            }
-            }
-        </script>
-        <logo>
-        	<a href="..\menu.php"><img src="..\imagem/logo.png" width=25%></a>
-        </logo>
-		<exit>
-        	<a href="..\logout.php"><img src="..\imagem/exit.png" width=50%></a>
-		</exit>
+				window.onclick = function(event) {
+				if (!event.target.matches('.dropbtn')) {
+					var dropdowns = document.getElementsByClassName("dropdown-content");
+					var i;
+					for (i = 0; i < dropdowns.length; i++) {
+					var openDropdown = dropdowns[i];
+					if (openDropdown.classList.contains('show')) {
+						openDropdown.classList.remove('show');
+					}
+					}
+				}
+				}
+			</script>
+            <logo>
+                <a href="..\menu.php"><img src="..\imagem/logo.png" width=25%></a>
+            </logo>
+            <exit>
+                <a href="..\logout.php"><img src="..\imagem/exit.png" width=50%></a>
+            </exit>
 		</div>
         <pag>
 			<h1>REGISTRO DE DEVOLUÇÕES</h1><p>
